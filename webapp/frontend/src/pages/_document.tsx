2bfcc4a5fb856b9fda44a750134977e8b50c7a5f
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-06-07 19:55:29 +0000

    add poppins font

--- webapp/frontend/src/pages/_document.tsx
+++ webapp/frontend/src/pages/_document.tsx
@@ -14,6 +14,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
